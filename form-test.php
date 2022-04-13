3932446abd8b73b27949845cd49fec6c06a9bf3c
VitaliyNosov <webdevelorestudio2017@gmail.com>
2022-04-13 16:48:50 +0300

    commit

--- form-test.php
+++ form-test.php
@@ -106,12 +106,10 @@ function mytable_activation_function()
     if ($wpdb->get_var("show tables like '" . owt_create_my_table() . "'") != owt_create_my_table()) {
 
         $mytable = 'CREATE TABLE `' . owt_create_my_table() . '` (
-                            `id` int(11) NOT NULL AUTO_INCREMENT,
                             `name` varchar(100) NOT NULL,
-                            `email` varchar(50) NOT NULL,
-                            `status` int(11) NOT NULL DEFAULT "1",
-                            `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                            PRIMARY KEY (`id`)
+                            `email` varchar(100) NOT NULL,
+                            `phone` varchar(100) NOT NULL,
+                            `date` varchar(100) NOT NULL
                           ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;';
 
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
